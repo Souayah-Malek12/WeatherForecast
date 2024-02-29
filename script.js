cab9939f53e8541abd7849cd44780c6c50fafcce
Souayah-Malek12 <malek.souayah@isimg.tn>
2024-02-29 11:35:35 +0100

    weather-app

---
+++ script.js
@@ -0,0 +1,39 @@
+const searchbtn = document.querySelector(".btnSearch");
+const weatherIcon = document.querySelector(".weather-icon");
+const cityName = document.querySelector("#input-city");
+
+const apikey = "9d1eaa7083ecd14b043f458b7b72a307";
+
+searchbtn.addEventListener("click", function dataRetrieve() {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName.value}&appid=${apikey}&units=metric`;
+
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      document.querySelector(".city").innerHTML = data.name;
+      document.querySelector(".temp").innerHTML = data.main.temp + "°C";
+      document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+      document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+      if (data.weather[0].main == "Clouds") {
+        weatherIcon.src = "images/clouds.png";
+      } else if (data.weather[0].main == "Clear") {
+        weatherIcon.src = "images/clear.png";
+      } else if (data.weather[0].main == "Rain") {
+        weatherIcon.src = "images/rain.png";
+      } else if (data.weather[0].main == "Drizzle") {
+        weatherIcon.src = "images/drizzle.png";
+      } else if (data.weather[0].main == "Mist") {
+        weatherIcon.src = "images/mist.png";
+      }
+
+    //   // Display error message if city not found
+    //   if (data.cod== 404) {
+    //     document.querySelector(".error").style.display = "block";
+    //     document.querySelector(".weather").style.display = "none";
+    //   } else {
+    //     document.querySelector(".weather").style.display = "block";
+    //     document.querySelector(".error").style.display = "none";
+    //   }
+    });
+});
